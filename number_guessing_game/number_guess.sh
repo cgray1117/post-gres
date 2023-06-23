badecfdc871653928a11e39bb9af588607d1f418
Chenese Gray <64931237+cgray1117@users.noreply.github.com>
2023-06-23 14:04:02 -0400

    Update number_guess.sh

--- number_guessing_game/number_guess.sh
+++ number_guessing_game/number_guess.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 echo "Enter your username:"
 read USERNAME
 
-USERNAME_RESULT=$($PSQL "SELECT  FROM games WHERE username='$USERNAME'")
+USERNAME_RESULT=$($PSQL "SELECT games_played, best_game_id, guess FROM users LEFT JOIN games ON users.best_game_id=games.game_id;")
+
 
 if [[ $USERNAME_RESULT ]]
 then
-    echo "Welcome back, $USERNAME! You have played <games_played> games, and your best game took <best_game> guesses."
+    echo $USERNAME_RESULT | while IFS="|" read GAMES_PLAYED BAR BEST_GAME_ID BAR GUESSES 
+    do 
+        echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $GUESSES guesses."
+    done
+    UPDATE_GAMES_PLAYED=$($PSQL "")
 else 
     echo "Welcome, $USERNAME! It looks like this is your first time here."
 fi
